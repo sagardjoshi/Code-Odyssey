1ae695a2d981f681ee924b18b8ea47a7bbf1c4c8
sagardjoshi <jo.sagar@gmail.com>
2024-12-24 15:12:17 -0800

    CloudKitchen assignment for order fulfilment

---
+++ CloudKitchen/m1d/java/gradle/src/main/java/com/css/challenge/client/Temperature.java
@@ -0,0 +1,7 @@
+package com.css.challenge.client;
+
+public class Temperature {
+    public static final String HOT = "hot";
+    public static final String COLD = "cold";
+    public static final String ROOM = "room";
+}
